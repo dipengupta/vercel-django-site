855f2b453ba7271e430816009ffb98010254f64f
dipengupta <dipenrgupta@gmail.com>
2023-07-09 11:26:24 -0400

    more static dirs

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -126,3 +126,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mysite/static')]
 
 #STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
