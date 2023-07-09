642a0648fb9cc1a9cb26c56930cc6db3ecf3b39f
dipengupta <dipenrgupta@gmail.com>
2023-07-09 16:54:06 -0400

    ch4

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -115,9 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#trying this out now
+'''
+trying this out now
+note: 194 static files copied to '/vercel/path0/staticfiles/static'
+'''
 #STATIC_URL = 'static/'
-STATIC_URL = os.path.join(BASE_DIR,'static/')
+STATIC_URL = 'staticfiles/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
