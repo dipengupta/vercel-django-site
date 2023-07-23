4538df312943e988d6136325427b1e40ae9596a6
dipengupta <dipenrgupta@gmail.com>
2023-07-23 11:38:02 -0400

    routes again

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -116,10 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/static'),)
 # Configures the staticfiles directory to serve
 # static files from /static/ on our deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # # Default primary key field type
