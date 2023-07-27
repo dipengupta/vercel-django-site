6b6aa3df14652825abfbc3f48cb042628b471e2b
dipeng <dipeng@urlinsgroup.com>
2023-07-27 14:52:52 -0400

    files

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_dir', 'static')
 STATIC_URL = '/static/'
 
 # # Default primary key field type
