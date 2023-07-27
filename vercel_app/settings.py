b619508097165c61b9611d452196992ec4049b53
dipeng <dipeng@urlinsgroup.com>
2023-07-27 08:45:17 -0400

    more staticfiles

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -117,20 +117,11 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# Configures the staticfiles directory to serve
-# static files from /static/ on our deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
-STATIC_URL = 'static/'
-
-# #
+STATIC_URL = 'staticfiles/'
 
 # # Default primary key field type
 # # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mysite/static')]
-
-# #STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# #STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
-# STATIC_ROOT = '/vercel/path0/staticfiles/static'
